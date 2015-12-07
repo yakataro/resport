e2312811963b2d7a3e50deb2f9019a46ff2279f0
yakataro <yamahiro149@gmail.com>
2015-12-07 16:01:39 +0900

    Update pageflip.js

--- pageflip.js
+++ pageflip.js
@@ -30,6 +30,16 @@
 	// List of all the page elements in the DOM
 	var pages = book.getElementsByTagName( "section" );
 	
+	// aタグを保持しておきます。
++	var anchors = book.getElementsByTagName( "a" );
++	
++	// pagesの数を再利用したいので変数に保持しておきます。
++	var pagesLength = pages.length;
++	
++	// canvasのz-indexを1にしておきます。
++	canvas.style.zIndex = "1";
++	
+	
 	// Organize the depth of our pages and create the flip definitions
 	for( var i = 0, len = pages.length; i < len; i++ ) {
 		pages[i].style.zIndex = len - i;
