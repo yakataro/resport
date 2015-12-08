3d3aed962ab170e375a025dbe1ca56fee0c80dd7
yakataro <yamahiro149@gmail.com>
2015-12-08 18:20:08 +0900

    test

---
+++ send.php
@@ -0,0 +1,26 @@
+<?php 
+//php.iniの編集ができない場合の言語とエンコード指定
+mb_language("japanese");
+mb_internal_encoding("utf-8");
+  
+if(!empty($_POST['mail'])){
+    $to=$_POST['mail'];
+    $name=$_POST['name'];
+    $msg=$_POST['msg'];
+
+ 
+
+ 
+  
+$success=mb_send_mail($to,$sub,"お名前：".$name."　本文：".$msg);
+}
+?>
+  
+  
+<?php 
+if($success){
+    echo('送信しました');
+}else{
+    echo('送信失敗！！');
+}
+?>
